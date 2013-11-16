cfef9c0055d3c7c1e909cd6612e7be577c0c98da
Ryan Griege <rgriege@sbcglobal.net>
2013-11-16 00:58:40 -0600

    Added insertion/deletion output

--- main.cpp
+++ main.cpp
@@ -48,29 +48,57 @@ void LCS_print_table(int* C, const char* x, const char* y)
 	}
 }
 
-void LCS_read_helper(int*& C, const char*& x, const char*& y, int i, int j, std::ostream& out)
+enum OutputMode {
+	NORMAL,
+	INSERTION,
+	DELETION,
+};
+
+void switch_from_mode(OutputMode mode, std::ostream& out)
+{
+	if (mode == INSERTION)
+		out << "+";
+	else if (mode == DELETION)
+		out << "-";
+	out << std::endl;
+}
+
+void LCS_read_helper(int*& C, const char*& x, const char*& y, int i, int j,
+					 std::ostream& out, OutputMode mode)
 {
 	if (i == 0 || j == 0) {
 		return;
 	} else if (x[i-1] == y[j-1]) {
+		if (mode != NORMAL)
+			switch_from_mode(mode, out);
 		out << x[i-1];
-		LCS_read_helper(C, x, y, i-1, j-1, out);
+		LCS_read_helper(C, x, y, i-1, j-1, out, NORMAL);
 	} else {
 		if (C[i*cy_len+(j-1)] > C[(i-1)*cy_len+j]) {
-			LCS_read_helper(C, x, y, i, j-1, out);
+			if (mode != INSERTION)
+				switch_from_mode(mode, out);
+			out << y[j-1];
+			LCS_read_helper(C, x, y, i, j-1, out, INSERTION);
 		} else {
-			LCS_read_helper(C, x, y, i-1, j, out);
+			if (mode != DELETION)
+				switch_from_mode(mode, out);
+			out << x[i-1];
+			LCS_read_helper(C, x, y, i-1, j, out, DELETION);
 		}
 	}
 }
 
 void LCS_read(int* C, const char* x, const char* y, int lcs_len, std::ostream& out)
 {
-	LCS_read_helper(C, x, y, x_len, y_len, out);
+	LCS_read_helper(C, x, y, x_len, y_len, out, NORMAL);
 }
 
 int main()
 {
+	/*const char* x = "abcfghijk";
+	const char* y = "abcdefgjk";*/
+	/*const char* x = "compress the size of the changes.";
+	const char* y = "compress anything.";*/
 	FILE* original_file = fopen("original.txt", "r");
 	char x[600];
 	int x_read = fread(x, 1, 600, original_file);
