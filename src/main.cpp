5fd06c41cafdc9d6680b6a704a4b5ec739c3179e
Ryan Griege <rgriege@sbcglobal.net>
2013-12-11 01:52:05 -0600

    adds blocking

--- src/main.cpp
+++ src/main.cpp
@@ -7,31 +7,99 @@
 #include "BlockedData.h"
 #undef max
 
-int cx_len;
-int cy_len;
-
-template <class T>
-int* LCS_compute_table(BlockedData<T>& x, BlockedData<T>& y)
+int* LCS_create_table(int x_len, int y_len)
 {
-    int* C = new int[cx_len*cy_len];
-    for (int i = 0; i <= x.length(); i++)
-        C[i*cy_len] = 0;
-    for (int j = 0; j <= y.length(); j++)
-        C[j] = 0;
-    for (int i = 1; i <= x.length(); i++) {
-        for (int j = 1; j <= y.length(); j++) {
-            if (x[i-1] == y[j-1])
-                C[i*cy_len+j] = C[(i-1)*cy_len+(j-1)] + 1;
-            else
-                C[i*cy_len+j] = std::max(C[i*cy_len+(j-1)], C[(i-1)*cy_len+j]);
-        }
-    }
-    return C;
+    int cx_len = x_len + 1;
+    int cy_len = y_len + 1;
+    return new int[cx_len*cy_len];
 }
 
-int LCS_length(int* C, int cx_len, int cy_len)
+template <class Iterator>
+int* LCS_create_table(Iterator x_begin, Iterator x_end, Iterator y_begin, Iterator y_end)
 {
-    return C[cx_len*cy_len-1];
+    int x_size = 1;
+    x_it = x_begin;
+    while(x_it != x_end) {
+        ++x_it;
+        ++x_size;
+    }
+
+    int y_size = 1;
+    y_it = y_begin;
+    while(y_it != y_end) {
+        ++y_it;
+        ++y_size;
+    }
+
+    return new int[x_size*y_size];
+}
+
+template <class T>
+void LCS_compute_table(T* x, int x_len, T* y, int y_len, int* table)
+{
+    int cx_len = x_len + 1;
+    int cy_len = y_len + 1;
+    for (int i = 0; i <= x_len; i++)
+        table[i*cy_len] = 0;
+    for (int j = 0; j <= y_len; j++)
+        table[j] = 0;
+    for (int i = 1; i <= x_len; ++i) {
+        for (int j = 1; j <= y_len; ++j) {
+            if (x[i-1] == y[i-1])
+                table[i*cy_len+j] = table[(i-1)*cy_len+(j-1)] + 1;
+            else
+                table[i*cy_len+j] = std::max(table[i*cy_len+(j-1)], table[(i-1)*cy_len+j]);
+        }
+    }
+}
+
+template <class T>
+void LCS_compute_table(BlockedData<T>& x, BlockedData<T>& y, int* table)
+{
+    int cx_len = x.length() + 1;
+    int cy_len = y.length() + 1;
+    for (int i = 0; i <= x.length(); i++)
+        table[i*cy_len] = 0;
+    for (int j = 0; j <= y.length(); j++)
+        table[j] = 0;
+    for (int i = 1; i <= x.length(); i++) {
+        for (int j = 1; j <= y.length(); j++) {
+            if (x[i-1] == y[i-1])
+                table[i*cy_len+j] = table[(i-1)*cy_len+(j-1)] + 1;
+            else
+                table[i*cy_len+j] = std::max(table[i*cy_len+(j-1)], table[(i-1)*cy_len+j]);
+        }
+    }
+}
+
+template <class T>
+void LCS_compute_table_blocked(T* x, int x_len, T* y, int y_len, int* table, int b)
+{
+    int cx_len = x_len + 1;
+    int cy_len = y_len + 1;
+    for (int i = 0; i <= x_len; i++)
+        table[i*cy_len] = 0;
+    for (int j = 0; j <= y_len; j++)
+        table[j] = 0;
+    for (int ii = 1; ii <= x_len; ii += b) {
+        int imax = min(ii+b, x_len);
+        for (int jj = 1; jj <= y_len; jj += b) {
+            int jmax = min(jj+b, y_len);
+            for (int i = ii; i <= imax; ++i) {
+                for (int j = jj; j <= jmax; ++j) {
+                    if (x[i-1] == y[i-1])
+                        table[i*cy_len+j] = table[(i-1)*cy_len+(j-1)] + 1;
+                    else
+                        table[i*cy_len+j] = std::max(table[i*cy_len+(j-1)], table[(i-1)*cy_len+j]);
+                }
+            }
+        }
+    }
+}
+
+int LCS_length(int* table, int cx_len, int cy_len)
+{
+    return table[cx_len*cy_len-1];
 }
 
 template <class T>
@@ -148,32 +216,34 @@ int main(int argc, char* argv[])
     if (output_stream == NULL)
         output_stream = &std::cout;
 
-    BlockedData<char> x(*original_input_stream, block_size);
+    int size = 10050;
+    char* x = new char[size];
+    original_input_stream->read(x, size);
+    x[original_input_stream->gcount()] = 0;
+    //BlockedData<char> x(*original_input_stream, block_size);
 
-    BlockedData<char> y(*modified_input_stream, block_size);
+    char* y = new char[size];
+    modified_input_stream->read(y, size);
+    y[modified_input_stream->gcount()] = 0;
+    //BlockedData<char> y(*modified_input_stream, block_size);
 
-    cx_len = x.length() + 1;
-    cy_len = y.length() + 1;
-
-    std::cout << "Comparing strings of size " << x.length() << " and " << y.length() << std::endl;
+    std::cout << "Comparing strings of size " << strlen(x) << " and " << strlen(y) << std::endl;
     long int start = GetTimeInMilliseconds();
-    int* C = LCS_compute_table(x, y);
+    int* table = LCS_create_table(strlen(x), strlen(y));
+    LCS_compute_table(x, strlen(x), y, strlen(y), table, block_size);
     long int end = GetTimeInMilliseconds();
     std::cout << "Op took " << end - start << " ms" << std::endl;
 
-    int lcs_len = LCS_length(C, cx_len, cy_len);
+    int lcs_len = LCS_length(table, strlen(x)+1, strlen(y)+1);
     std::cout << "LCS Length: " <<  lcs_len << std::endl;
 
-    std::ostringstream out;
+    /*std::ostringstream out;
     LCS_read(C, x, y, out);
     std::string str = out.str();
     std::cout << "LCS Sequence:" <<std::endl;
     for (std::string::reverse_iterator it = str.rbegin(); it != str.rend(); ++it)
-        std::cout << *it;
+    std::cout << *it;*/
 
     //LCS_print_table(C, x, y);
-
-    std::cout << "Press ENTER to exit...";
-    char line[1];
-    std::cin.getline(line, 1);
+    delete[] table;
 }
